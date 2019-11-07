533ac1e969ba2cc2410209cb7c624738a83f282b
Jeff Lewis <mail@jeffreylewis.me>
2019-11-07 09:51:54 -0600

    Fix tests

---
+++ testSetup.js
@@ -0,0 +1,11 @@
+jest.mock("react-native-socket-mobile", () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  inFocusDisplaying: jest.fn(),
+  updateStatusFromDevices: jest.fn(),
+  start: jest.fn(),
+  stop: jest.fn(),
+  clearAllListeners: jest.fn()
+}));
+
+jest.mock("react-native-sound-player", () => jest.fn());
