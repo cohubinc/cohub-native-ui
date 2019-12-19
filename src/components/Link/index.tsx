c2d8f8ea4b463739fd52a2cbc365aef906be2658
brayhoward <brandonrhoward@gmail.com>
2019-12-18 20:53:45 -0600

    Add hitslop to link

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,48 +1,63 @@
 import React from "react";
-import { Text, TextProps, Linking, TextInput, View } from "react-native";
+import { Linking, TouchableOpacity, TouchableOpacityProps } from "react-native";
 
-import Color from "../../definitions/enums/Color";
-import gs from "../../definitions/constants/GlobalStyles";
+import Color from "src/definitions/enums/Color";
+import Typography from "src/components/Typography";
 
-interface IProps extends TextProps {
+interface IProps extends TouchableOpacityProps {
   children: React.ReactNode;
   muted?: boolean;
   href?: string;
+  /** @default true */
+  underlined?: boolean;
 }
 
-export default function Link({
-  children,
-  style,
-  muted,
-  href,
-  onPress,
-  ...restOfProps
-}: IProps) {
+export default function Link(props: IProps) {
+  const {
+    children,
+    style,
+    muted,
+    href,
+    onPress,
+    underlined = true,
+    ...restOfProps
+  } = props;
+
+  let hitSlop = props.hitSlop || 5;
+  if (typeof hitSlop === "number") {
+    hitSlop = { top: hitSlop, left: hitSlop, bottom: hitSlop, right: hitSlop };
+  }
+
   return (
-    <TextInput
+    <TouchableOpacity
       {...restOfProps}
-      editable={false}
-      onTouchStart={e => {
+      hitSlop={hitSlop}
+      onPress={e => {
         onPress && onPress(e);
 
         if (href) {
           Linking.openURL(href).catch(err =>
-            console.log("An error occurred in Link", err)
+            console.log("An error occurred in Link", href, err)
           );
         }
       }}
       style={[
-        gs.regularBodyText,
-        {
-          color: (muted ? Color.darkGrey : Color.primaryGreen) as any,
+        underlined && {
           borderBottomWidth: 1,
-          borderColor: Color.primaryGreen as any,
-          alignSelf: "flex-start"
+          borderColor: Color.primaryGreen as any
         },
         style
       ]}
     >
-      {children}
-    </TextInput>
+      <Typography
+        style={[
+          {
+            color: (muted ? Color.darkGrey : Color.primaryGreen) as any
+          }
+        ]}
+      >
+        {children}
+      </Typography>
+    </TouchableOpacity>
   );
 }
