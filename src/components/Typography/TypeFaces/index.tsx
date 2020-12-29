143dbd671a4a6ea5f396f86066a9e4f94ba7b2a2
Brandon R. Howard <brandonrhoward@gmail.com>
2020-12-29 12:08:05 -0600

    'Use Typography as base cmpt in Link' (#63)

--- src/components/Typography/TypeFaces/index.tsx
+++ src/components/Typography/TypeFaces/index.tsx
@@ -2,38 +2,38 @@ import { typographyFactory } from "./typographyFactory";
 
 export const SuperTitle = typographyFactory({
   "data-qa": "page-supertitle",
-  style: { fontSize: 48 }
+  style: { fontSize: 48 },
 });
 
 export const Title = typographyFactory({
   "data-qa": "page-title",
-  style: { fontSize: 36 }
+  style: { fontSize: 36 },
 });
 
 export const Subtitle = typographyFactory({
   "data-qa": "page-subtitle",
-  style: { fontSize: 30 }
+  style: { fontSize: 30 },
 });
 
 export const HeadingLarge = typographyFactory({
   "data-qa": "heading-large",
-  style: { fontSize: 24 }
+  style: { fontSize: 24 },
 });
 
 export const HeadingSmall = typographyFactory({
   "data-qa": "heading-small",
-  style: { fontSize: 20 }
+  style: { fontSize: 20 },
 });
 
 export const HeadingTiny = typographyFactory({
   "data-qa": "heading-tiny",
-  style: { fontSize: 18 }
+  style: { fontSize: 18 },
 });
 
 export const Large = typographyFactory({ style: { fontSize: 16 } });
 
 export const Regular = typographyFactory({
-  style: { fontSize: 14 }
+  style: { fontSize: 14 },
 });
 
 export const Small = typographyFactory({ style: { fontSize: 12 } });
